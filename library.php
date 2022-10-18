1aa724fa5424621570cd8dcdeee3078ad9b5d51a
mirai <nakajimami11@gmail.com>
2022-10-19 02:28:26 +0900

    デプロイ

--- library.php
+++ library.php
@@ -6,7 +6,7 @@ function h($value){
 
 //dbへの接続
 function dbconnect(){
-    $db = new mysqli('localhost','root','root','mybbs');//dbに接続
+    $db = new mysqli('mysql:heroku_6a1f0d0050fbc9b;host=us-cdbr-east-06.cleardb.net; charset=utf8', 'b5aaf9d54ebf04', 'dc5e6fcf');
     if(!$db){//上手く接続できなかった時エラーに出力
 		die($db->error);
 	}
