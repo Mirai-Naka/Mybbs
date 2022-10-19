1cb8e711e16e4e89c2850bdc16d71f0253078fdb
mirai <nakajimami11@gmail.com>
2022-10-20 00:13:27 +0900

    時間調整

--- library.php
+++ library.php
@@ -7,6 +7,7 @@ function h($value){
 //dbへの接続
 function dbconnect(){
     $db = new mysqli('us-cdbr-east-06.cleardb.net', 'b5aaf9d54ebf04', 'dc5e6fcf', 'heroku_6a1f0d0050fbc9b');
+    $db->query("SET time_zone = '+09:00'");
     if(!$db){//上手く接続できなかった時エラーに出力
 		die($db->error);
 	}
