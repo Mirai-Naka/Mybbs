f27c2b10a8292ecf3492d9c8c264c18e9edea844
mirai <nakajimami11@gmail.com>
2022-10-19 19:22:04 +0900

    DB修正

--- library.php
+++ library.php
@@ -6,7 +6,7 @@ function h($value){
 
 //dbへの接続
 function dbconnect(){
-    $db = new mysqli('mysql:heroku_6a1f0d0050fbc9b;host=us-cdbr-east-06.cleardb.net; charset=utf8', 'b5aaf9d54ebf04', 'dc5e6fcf');
+    $db = new mysqli('localhost','root','root','my_bbs');
     if(!$db){//上手く接続できなかった時エラーに出力
 		die($db->error);
 	}
